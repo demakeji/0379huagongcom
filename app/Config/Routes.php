000cd20ae725f04c86620e41f04595c64a5d28bd
liuwenlong <270176349@qq.com>
2026-02-25 15:52:22 +0800

    fix(routes):添加路由项目

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,31 @@
 <?php
 
+use App\Controllers\Hproducts;
+use App\Controllers\News;
+use App\Controllers\Pages;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'home::index');
+$routes->get('/', [Hproducts::class, 'index']);
+$routes->get('hproducts', [Hproducts::class, 'index']);
+$routes->get('hproducts/(:segment)', [Hproducts::class, 'show']);
+$routes->get('hproducts/detail/(:num)', [[Hproducts::class, 'detail'], '$1']);
+
+$routes->get('news', 'News::index');
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']); // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']);
+
+$routes->get('profile', 'Profile::index');
+
+$routes->get('feedback', 'Feedback::index');
+
+$routes->get('contact', 'Contact::index');
+
+$routes->get('pages', [Pages::class, 'view']);
+$routes->get('(:segment)', [Pages::class, 'view']);
+
+
